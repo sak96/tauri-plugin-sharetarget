c44418928abb89542159b9730479e7e2eb62be79
lafleur <lafleur@boum.org>
2024-11-02 02:25:55 +0100

    fix docs.rs fail building docs

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 const COMMANDS: &[&str] = &["ping", "registerListener"];
 
 fn main() {
-  tauri_plugin::Builder::new(COMMANDS)
-    .android_path("android")
-    .ios_path("ios")
-    .build();
+    // Do not build when processed by docs.rs.
+    if std::env::var("DOCS_RS").is_err() {
+        tauri_plugin::Builder::new(COMMANDS)
+            .android_path("android")
+            .ios_path("ios")
+            .build();
+    }
 }
