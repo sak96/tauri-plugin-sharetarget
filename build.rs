66814f42653a1d1bf8ab4b4a145eb36c220a3772
lafleur <lafleur@boum.org>
2024-11-02 18:18:39 +0100

    fix docs.rs 2: only do build.rs when cfg(not(docsrs))

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
+#[cfg(not(docsrs))]
 const COMMANDS: &[&str] = &["ping", "registerListener"];
 
 fn main() {
     // Do not build when processed by docs.rs.
-    if std::env::var("DOCS_RS").is_err() {
+    #[cfg(not(docsrs))]
+    {
         tauri_plugin::Builder::new(COMMANDS)
             .android_path("android")
             .ios_path("ios")
