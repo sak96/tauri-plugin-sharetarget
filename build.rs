40c7bbc784d659d46f7c2b3116db782471db23be
lafleur <lafleur@boum.org>
2024-11-02 19:39:05 +0100

    fix docsrs 3: show only README, more useful

--- build.rs
+++ build.rs
@@ -1,13 +1,9 @@
-#[cfg(not(docsrs))]
 const COMMANDS: &[&str] = &["ping", "registerListener"];
 
 fn main() {
     // Do not build when processed by docs.rs.
-    #[cfg(not(docsrs))]
-    {
-        tauri_plugin::Builder::new(COMMANDS)
-            .android_path("android")
-            .ios_path("ios")
-            .build();
-    }
+    tauri_plugin::Builder::new(COMMANDS)
+        .android_path("android")
+        .ios_path("ios")
+        .build();
 }
